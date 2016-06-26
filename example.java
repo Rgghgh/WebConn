0d9464dd72eeffff9bf53a65704f23f1dbec429e
Rgghgh <tomer1091@gmail.com>
2016-06-26 19:14:49 +0300

    Update example.java

--- example.java
+++ example.java
@@ -1,41 +1,34 @@
-public class ChatActivity extends Activity implements View.OnClickListener, OnWebResultListener
+package example
+
+// some imports ...
+
+public class ExampleActivity extends Activity implements OnWebResultListener
 {
-    private ArrayList<Message> msgs;
-    private MessageAdapter adapter;
-
-    private String myNumber;
-    private String toNumber;
-
-    private ListView lvChat;
-    private EditText etMsg;
-    private Button btnSend;
-
-    private BroadcastReceiver receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         // SOME CODE
+        
+        makeAWebRequest("http://exmaple.com?optionalGet=value"); // http or https
     }
 
-    @Override
-    public void onClick(View v)
+    public void makeAWebRequest(String url)
     {
         String text = this.etMsg.getText().toString();
         this.etMsg.setText("");
 
-        WebRequest request = new WebRequest(0, "http://exmaple.com?optionalGet=value");
-        request.setOnWebResultListener(this);
-        request.setPostQuery("post1=value1&post2=value2");
+        WebRequest request = new WebRequest(0, url); 
+        // 0 is the requestCode. Can be used to indetify a request in the Result listener when more than one is made at once.
+        request.setOnWebResultListener(this); // this implements OnWebResultListener interface
+        request.setPostQuery("post1=value1&post2=value2"); // optional...
         WebThread.handle(request);
     }
 
     @Override
     public void onWebResult(int requestCode, String result)
     {
-        this.msgs.add(new Message(result, true));
-        this.adapter.notifyDataSetChanged();
+        log.d("WebResult", result);
     }
 }
