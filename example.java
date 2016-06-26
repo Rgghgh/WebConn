72feb9e6a15152e672fa09dabbfce532366d157d
Rgghgh <tomer1091@gmail.com>
2016-06-26 19:16:08 +0300

    Update example.java

--- example.java
+++ example.java
@@ -16,9 +16,6 @@ public class ExampleActivity extends Activity implements OnWebResultListener
 
     public void makeAWebRequest(String url)
     {
-        String text = this.etMsg.getText().toString();
-        this.etMsg.setText("");
-
         WebRequest request = new WebRequest(0, url); 
         // 0 is the requestCode. Can be used to indetify a request in the Result listener when more than one is made at once.
         request.setOnWebResultListener(this); // this implements OnWebResultListener interface
